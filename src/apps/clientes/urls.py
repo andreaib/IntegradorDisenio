e3c8b6dcb6f0ec978b08edf4e52f7fc22ed88b41
lucassebib <lucas.sebib@gmail.com>
2018-12-16 19:00:07 -0300

    asd

--- src/apps/clientes/urls.py
+++ src/apps/clientes/urls.py
@@ -6,4 +6,5 @@ app_name = "clientes"
 urlpatterns = [
 	url(r'^Nuevo/', views.Crear.as_view(), name='crear'),
 	url(r'^listar/', views.Listar.as_view(), name='listar'),
+	url(r'^new/', views.NuevoCliente, name='new'),
 ]
--- src/apps/clientes/views.py
+++ src/apps/clientes/views.py
@@ -1,7 +1,10 @@
+import json
+
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 from django.urls import reverse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 
 from .models import Clientes
 from .forms import formClientes
@@ -18,3 +21,47 @@ class Crear(CreateView):
 
 	def get_success_url(self, **kwargs):
 		return reverse('clientes:listar')
+
+
+def NuevoCliente(request):
+	if request.is_ajax():
+		try:		
+			id_cliente = int(request.GET.get('id',None))
+			first_name = request.GET.get('nombre',None)
+			last_name = request.GET.get('apellido',None)
+			email = request.GET.get('email',None)
+			fecha_ingreso = request.GET.get('fe_ingreso',None)
+			fecha_nacimiento = request.GET.get('id',None)
+			cuil = request.GET.get('dni',None)
+			sexo = request.GET.get('sexo',None)
+			peso = request.GET.get('id',None)
+			estatura = request.GET.get('estatura',None)
+			username = first_name.lower() + last_name.lower()
+		except Exception as e:
+			print("************")
+			print(str(e))
+
+		estado = 'ok'
+		try:
+			c = Clientes.objects.create(
+				#pk=id_cliente,
+				username=username,
+				first_name=first_name,
+				last_name=last_name,
+				email=email, 
+				#fecha_ingreso=fecha_ingreso,
+				#fecha_nacimiento=fecha_nacimiento, 
+				cuil=cuil, 
+				peso=peso, 
+				estatura=estatura				
+				)
+		except Exception as e:
+			estado='mal'
+			print("***************")
+			print(str(e))
+		
+		data = json.dumps(estado)
+		mimetype = 'application/json'
+		return HttpResponse(data, mimetype)
+	else:
+		return Http404
